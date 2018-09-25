942fb415335a38a9c3ef3e06aa69da1707324d99
Juan Felipe <juan9505@gmail.com>
2018-09-24 22:01:51 -0500

    Resolve dependencies errors on Android. Update android graddle dependencies. Add fonts.

--- android/app/src/main/java/com/cheffiiapp/MainApplication.java
+++ android/app/src/main/java/com/cheffiiapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.cheffiiapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
+            new RNCameraPackage(),
             new ReactNativeI18n(),
             new VectorIconsPackage()
       );
