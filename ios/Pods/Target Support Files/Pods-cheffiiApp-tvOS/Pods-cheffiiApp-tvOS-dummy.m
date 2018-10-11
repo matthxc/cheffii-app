5559298485c68f971582ef884957f1431e1958a7
Juan Felipe <juan9505@gmail.com>
2018-10-10 23:20:45 -0500

    Revert "Configure ios project. Fix bugs on ios project libraries, dependencies, cocoapods. Update dependencies."
    This reverts commit c503b338139bb284dee2b85eca2d5fbbca6fd033.

--- ios/Pods/Target Support Files/Pods-cheffiiApp-tvOS/Pods-cheffiiApp-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cheffiiApp_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_cheffiiApp_tvOS
-@end
--- ios/Pods/Target Support Files/Pods-cheffiiApp/Pods-cheffiiApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cheffiiApp : NSObject
-@end
-@implementation PodsDummy_Pods_cheffiiApp
-@end
